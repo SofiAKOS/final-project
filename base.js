c7226591f61b89e6b86e22e5dabaae1f5f728949
SofiAKOS <123480309+SofiAKOS@users.noreply.github.com>
2023-06-14 16:10:59 -0700

    Comment

---
+++ base.js
@@ -0,0 +1,6 @@
+//вивід текстового повідомлення в консоль
+comsole.log ("Message from another js file");
+console.warn("Some warning message");
+console.error("Some error message");
+let email = "kossofia@gmail.com";
+console.log("My email:"+ email);
