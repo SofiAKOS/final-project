d1fd2849b71f78667402664425539efc4fb38894
SofiAKOS <123480309+SofiAKOS@users.noreply.github.com>
2023-06-26 19:02:20 -0700

    Comment

---
+++ 01 base/loopsfunctions.js
@@ -0,0 +1,12 @@
+function createArray(start, end) {
+    let array = [];
+    for (let i = start; i <= end; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+  
+  let arr = createArray(2, 9);
+  console.log(arr);
+
+  
